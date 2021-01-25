78aef8cc2f7df5d5d160eea90b58498937276657
John Nielsen <jeppehilmer@gmail.com>
2021-01-25 09:57:21 +0100

    Update create.js

--- create.js
+++ create.js
@@ -106,7 +106,7 @@ var task = function(win, info, settings, no, callback) {
                       '#!/bin/bash \n' +
                       'sudo apt-get install squid wget apache2-utils -y \n' +
                       `sudo htpasswd -b -c /etc/squid/passwd proxydrip ${info.password} \n` +
-                      'wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/mikelucid/proxydrip/master/confg/userpass/squid.conf --no-check-certificate \n' +
+                      'wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/JohnNielse/proxydrip/master/confg/userpass/squid.conf --no-check-certificate \n' +
                       'systemctl restart squid.service && systemctl enable squid.service \n' +
                       `iptables -I INPUT -p tcp --dport ${info.port} -j ACCEPT \n` +
                       'iptables-save'
